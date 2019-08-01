751764db1e2eedca188b49c80b2a17d80f17e8c7
stephaneetienne <stephaneetienne70@gmail.com>
2019-08-01 03:56:53 -0400

    initial commit

--- app/src/main/java/com/example/week3day2/MainActivity.java
+++ app/src/main/java/com/example/week3day2/MainActivity.java
@@ -1,9 +1,11 @@
 package com.example.week3day2;
 
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity  implements BlankFragment.OnFragmentInteractionListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -11,5 +13,14 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         //comment
         //comment again
+
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.fragmentPlaceholder, new BlankFragment());
+        fragmentTransaction.commit();
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
     }
 }
